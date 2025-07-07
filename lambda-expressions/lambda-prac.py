4e81dd3a0e5f26e4726985e7d6691fa57450a7ee
Adityaypatil <adityapatil961992@gmail.com>
2025-07-07 12:26:56 +0530

    Lambda expression code added

--- lambda-expressions/lambda-prac.py
+++ lambda-expressions/lambda-prac.py
@@ -12,3 +12,7 @@ for i in ans:
 ans = lambda x:"Even" if x%2 == 0 else "Odd"
 print(ans(4))
 
+# Example: Filter even numbers from a list
+n = [1, 2, 3, 4, 5, 6]
+even = filter(lambda x: x % 2 == 0, n)
+print(list(even))
